2f6d45a55d837d5c97f3accb3131c6a63ec064ec
Christof Küstner <kuestner@mfk.fau.de>
2013-08-27 16:26:54 +0200

    context.py: joining paths in the python way

--- tests/context.py
+++ tests/context.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-rootDir = os.path.dirname(os.path.abspath(__file__)) + "/../"
+rootDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 sys.path.insert(0, rootDir)
 
 import iknow
