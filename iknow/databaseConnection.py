3284fcb838f3b6facb9314c9c9967978f45bfc5d
Jan Kallwies <jan.kallwies@unibw.de>
2013-12-03 09:18:02 +0100

    WIP -- Experiments with Indexes and CodernityDB

--- iknow/databaseConnection.py
+++ iknow/databaseConnection.py
@@ -1,18 +1,43 @@
 # -*- coding: utf-8 -*-
 
+#import CodernityDB
 from CodernityDB.database import Database
-import CodernityDB
+from CodernityDB.hash_index import HashIndex
 import logging
+
+from hashlib import md5
+
+class TagIndex(HashIndex):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['key_format'] = '16s'
+        super(TagIndex, self).__init__(*args, **kwargs)
+
+    def make_key_value(self, data):
+        if data['_t'] == 'tag':
+            tagName = data['name']
+            # if not isinstance(login, basestring):
+            #     login = str(login)
+            return md5(tagName).digest(), {'name': data['name'], 'parents': data['parents']}
+
+    def make_key(self, key):
+        return md5(key).digest()
 
 
 class DatabaseConnection:
     def __init__(self, path="./noSQLDB"):
         self.db = Database(path)
 
-        try:
-            self.db.create()
-        except CodernityDB.index.IndexConflict:
+        tagsIndex = TagIndex(self.db.path, 'tags')
+
+        print(tagsIndex)
+
+        if self.db.exists():
             self.db.open()
+            self.db.reindex()
+        else:
+            self.db.create()
+            #self.db.add_index(tagsIndex)
 
         """
         print("********************************")
